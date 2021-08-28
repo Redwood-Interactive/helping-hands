b374e88794d2fe127d8256a1bbf5b222c7a7aa6a
katscap <katielaw97@gmail.com>
2021-08-28 15:50:53 -0400

    remove logs

--- server/passport/passport-setup.js
+++ server/passport/passport-setup.js
@@ -25,8 +25,6 @@ passport.use(
   new GoogleStrategy(optionsG,
     (token, tokenSecret, profile, done) => {
       let { sub, given_name, family_name, picture } = profile._json
-
-      console.log('this is the profile', profile)
       authenticateLogin.authenticate(sub)
         .then((data) => {
           if (data.rows[0]) {
