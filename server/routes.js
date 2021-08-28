25e470e895af197e8071a30f730bc5556875bfe5
katscap <katielaw97@gmail.com>
2021-08-28 16:48:08 -0400

    logout

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
-const { newCustomer } = require('./controllers/index.js');
+const { newCustomer, postFeedback } = require('./controllers/index.js');
 const router = require('express').Router();
-const passport = require('passport')
+const passport = require('passport');
 
 router.get('/newcustomer', newCustomer.get);
 
@@ -9,18 +9,22 @@ router.get('/newcustomer', newCustomer.get);
 //AUTHENTICATION
 router.get('/auth/google', passport.authenticate('google', {
   scope:['profile', 'email']
-}))
+}));
 
 router.get('/auth/google/redirect', passport.authenticate('google'), (req, res) => {
   // res.redirect('/')
-  res.redirect('/')
-})
+  res.send(req.user)
+});
 
-router.get('auth/logout', (req, res) => {
-  req.logout();
-  res.redirect('/')
-})
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/');
+});
 
-router.post('/feedback', postFeedback.post)
+router.post('/feedback', postFeedback.post);
 
 module.exports = router;
+
+// express:sess=eyJwYXNzcG9ydCI6eyJ1c2VyIjo0fX0=; express:sess.sig=k8IWsh-ajezo9oByZcAOAss1YNc
+
+// express:sess=eyJwYXNzcG9ydCI6e319; express:sess.sig=j4PmiSSD8LCPwlEfO0pb_uj0gZ4
