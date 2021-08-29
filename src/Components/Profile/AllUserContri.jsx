be4c053d49c24810570e02e75b205f16545a0df6
SandraM1 <sministro@gmail.com>
2021-08-29 19:13:52 -0400

    user profile home page

---
+++ src/Components/Profile/AllUserContri.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { ItemsContainer, Title, ItemsProducts, Item, ItemImage, TextDiv, Location, ImageDiv, CategoryDiv, CategoryIcon, ContributionTitle, Time } from '../Contributions/Styles/Items.style.js';
+
+const AllUserContributions = ( { contributions } ) => {
+  return (
+    <ItemsContainer>
+      <ItemsProducts>
+        {contributions.map((item, index) =>
+          <Item key={index}>
+            <ImageDiv>
+              <ItemImage src={item.photos}/>
+              <CategoryDiv>
+                <CategoryIcon>∆</CategoryIcon>
+              </CategoryDiv>
+            </ImageDiv>
+            <TextDiv>
+              <Title>{item.title}</Title>
+              <Location>Location</Location>
+              <Time>5m ago</Time>
+            </TextDiv>
+          </Item>
+        )}
+      </ItemsProducts>
+    </ItemsContainer>
+  );
+}
+
+export default AllUserContributions;
