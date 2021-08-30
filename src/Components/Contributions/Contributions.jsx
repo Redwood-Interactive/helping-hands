5be812b2b9de051c6000d6b4bd0e6ebd767bddff
Seiji Matsumoto <seijim27@gmail.com>
2021-08-30 12:11:05 -0400

    Added axios request to get contributions

--- src/Components/Contributions/Contributions.jsx
+++ src/Components/Contributions/Contributions.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ContributionsContainer, ContributionTitle, ContainerDiv } from './Styles/Contributions.style.js'
 import Sidebar from './Sidebar.jsx';
 import Items from './Items.jsx';
+import axios from 'axios';
 
 const Contributions = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getContributions();
+  }, [])
+
+  const getContributions = () => {
+    axios.get('/getcontributions')
+      .then(res => {
+        console.log(res.data);
+      })
+  }
+
   return (
     <ContributionsContainer>
       <ContributionTitle>Contributions</ContributionTitle>
