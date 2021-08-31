24e4cdd51493b948a2792e83a6c66d08c1fef457
June Lee <junehaklee94@gmail.com>
2021-08-31 15:07:08 -0500

    fixed some bugs

--- server/models/getContributions.js
+++ server/models/getContributions.js
@@ -8,10 +8,10 @@ module.exports = {
     var query = `
       SELECT contributions.id, contributions.title, contributions.c_description, contributions.category, contributions.c_date, contributions.condition, contributions.available, contributions.for_free, JSON_BUILD_OBJECT('user_id', users.id,'first_name', users.first_name, 'last_name', users.last_name, 'current_rating', users.current_rating, 'email', users.email, 'phone', users.phone, 'profile_pic', users.profile_pic, 'auth_id', users.auth_id) AS user, ARRAY_AGG(photos.photo_url) AS photos, JSON_BUILD_OBJECT('street_name', locations.street_name, 'city', locations.city, 'state', locations.state, 'zipcode', locations.zipcode) AS location
       FROM contributions
-      WHERE contributions.available = true
       LEFT JOIN photos ON contributions.id = photos.contribution_id
       LEFT JOIN users ON contributions.user_id = users.id
       LEFT JOIN locations ON contributions.user_id = locations.user_id
+      WHERE contributions.available = true
       GROUP BY contributions.id, users.id, locations.street_name, locations.city, locations.state, locations.zipcode
       ORDER BY contributions.c_date DESC
       LIMIT 20
