3c6468f4977c360a9dbf2647edf8e9babb636079
June Lee <junehaklee94@gmail.com>
2021-09-01 09:32:05 -0500

    added streetName, city, state, and zipcode to the form

--- src/Components/Contributions/ItemsModalCarousel.jsx
+++ src/Components/Contributions/ItemsModalCarousel.jsx
@@ -21,10 +21,6 @@ const ItemsModalCarousel = ({ clickedimages }) => {
               src={image}
             />
           </ItemsImageContainer>
-          {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
         </Carousel.Item>
       )}
     </Carousel>
