3bb8d4820f1f9e311c5af45d173b971041a5101d
Seiji Matsumoto <seijim27@gmail.com>
2021-09-01 10:47:23 -0400

    Added filtering by condition

--- src/Components/Contributions/Items.jsx
+++ src/Components/Contributions/Items.jsx
@@ -60,39 +60,75 @@ const Items = (props) => {
               </Item>
             )
           }) :
-            props.data.map((item, index) => { // filtered by categories
-              if (props.categories.indexOf(item.category) !== -1) {
-                return (
-                  <Item key={index} onClick={() => {
-                    setShowModal(true)
-                    setClickedImages(item.photos)
-                    setTitle(item.title)
-                    setDescription(item.c_description)
-                    setCategory(item.category)
-                    setLocation(item.location.city + ', ' + item.location.state)
-                    setClass(icons[item.category])
-                    setUser(item.user)
-                    setCondition(item.condition)
-                  }}>
-                    <ImageDiv>
-                      <ItemImage src={item.photos[0]} />
-                      <CategoryDiv></CategoryDiv>
-                      <CategoryIcon className={icons[item.category]}></CategoryIcon>
-                    </ImageDiv>
-                    <TextDiv>
-                      <Title>{item.title}</Title>
-                      <AnotherDiv>
-                        <Location>{item.location.city}, {item.location.state}</Location>
-                        <BottomRow>
-                          <Time>5m ago</Time>
-                          <CategoryName>{item.category}</CategoryName>
-                        </BottomRow>
-                      </AnotherDiv>
-                    </TextDiv>
-                  </Item>
-                )
-              }
-            })
+            props.categories.length && props.conditions.length ?
+              props.data.map((item, index) => { // filtered by categories
+                if (props.categories.indexOf(item.category) !== -1 && props.conditions.indexOf(item.condition) !== -1) {
+                  console.log('1');
+                  return (
+                    <Item key={index} onClick={() => {
+                      setShowModal(true)
+                      setClickedImages(item.photos)
+                      setTitle(item.title)
+                      setDescription(item.c_description)
+                      setCategory(item.category)
+                      setLocation(item.location.city + ', ' + item.location.state)
+                      setClass(icons[item.category])
+                      setUser(item.user)
+                      setCondition(item.condition)
+                    }}>
+                      <ImageDiv>
+                        <ItemImage src={item.photos[0]} />
+                        <CategoryDiv></CategoryDiv>
+                        <CategoryIcon className={icons[item.category]}></CategoryIcon>
+                      </ImageDiv>
+                      <TextDiv>
+                        <Title>{item.title}</Title>
+                        <AnotherDiv>
+                          <Location>{item.location.city}, {item.location.state}</Location>
+                          <BottomRow>
+                            <Time>5m ago</Time>
+                            <CategoryName>{item.category}</CategoryName>
+                          </BottomRow>
+                        </AnotherDiv>
+                      </TextDiv>
+                    </Item>
+                  )
+                }
+              }) :
+              props.data.map((item, index) => { // filtered by categories
+                if (props.categories.indexOf(item.category) !== -1 || props.conditions.indexOf(item.condition) !== -1) {
+                  console.log('1');
+                  return (
+                    <Item key={index} onClick={() => {
+                      setShowModal(true)
+                      setClickedImages(item.photos)
+                      setTitle(item.title)
+                      setDescription(item.c_description)
+                      setCategory(item.category)
+                      setLocation(item.location.city + ', ' + item.location.state)
+                      setClass(icons[item.category])
+                      setUser(item.user)
+                      setCondition(item.condition)
+                    }}>
+                      <ImageDiv>
+                        <ItemImage src={item.photos[0]} />
+                        <CategoryDiv></CategoryDiv>
+                        <CategoryIcon className={icons[item.category]}></CategoryIcon>
+                      </ImageDiv>
+                      <TextDiv>
+                        <Title>{item.title}</Title>
+                        <AnotherDiv>
+                          <Location>{item.location.city}, {item.location.state}</Location>
+                          <BottomRow>
+                            <Time>5m ago</Time>
+                            <CategoryName>{item.category}</CategoryName>
+                          </BottomRow>
+                        </AnotherDiv>
+                      </TextDiv>
+                    </Item>
+                  )
+                }
+              })
           : null}
       </ItemsProducts>
       <ItemsModal show={showModal} onHide={() => setShowModal(false)} clickedimages={clickedimages} title={title} description={description} category={category} location={location} iconclass={iconClass} user={user} condition={condition} />
