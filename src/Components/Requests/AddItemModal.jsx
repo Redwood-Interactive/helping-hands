b01e373350b191f2c9e7bf2421d10ac65cfed080
June Lee <junehaklee94@gmail.com>
2021-09-01 14:24:38 -0500

    fixed request model page

--- src/Components/Requests/AddItemModal.jsx
+++ src/Components/Requests/AddItemModal.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal, Button, ModalDialog, ModalHeader, ModalTitle, ModalBody, ModalFooter, Form } from 'react-bootstrap';
-import { FormContainer, UpperHalf, LeftSide, RightSide, LowerHalf } from '../Contributions/Styles/AddItemModal.style.js';
+import { FormContainer, UpperHalf, LeftSide, RightSide, LowerHalf, MidHalf, MainAddress, City, State, ZipCode, TitleContainer, CheckDiv, Title } from '../Contributions/Styles/AddItemModal.style.js';
 
 const AddItemModal = (props) => {
   const [title, setTitle] = useState('')
@@ -9,6 +9,12 @@ const AddItemModal = (props) => {
   const [condition, setCondition] = useState('')
   const [location, setLocation] = useState('')
   const [description, setDescription] = useState('')
+
+  useEffect(() => {
+    if (props.userInfo.locations) {
+
+    }
+  }, [props.userInfo])
 
   const submitItem = () => {
     let form = {
@@ -20,9 +26,9 @@ const AddItemModal = (props) => {
 
     }
     axios.post('something', form)
-         .then((res) => {
-           console.log(res)
-         })
+      .then((res) => {
+        console.log(res)
+      })
   }
 
   return (
@@ -35,22 +41,26 @@ const AddItemModal = (props) => {
     >
 
       <Modal.Body>
-        <Form>
-          <FormContainer>
+        <FormContainer>
+          <Form>
             <UpperHalf>
               <LeftSide>
-                <Form.Group className="mb-3" controlId="formBasicTitle">
-                  <Form.Label>Title</Form.Label>
-                  <Form.Control onChange={(e) => setTitle(e.target.value)} type="text" maxLength='20' required placeholder="Enter title" />
-                </Form.Group>
+                <TitleContainer>
+                  <Title>
+                    <Form.Group className="mb-3" controlId="formBasicTitle">
+                      <Form.Label>Title</Form.Label>
+                      <Form.Control onChange={(e) => setTitle(e.target.value)} type="text" maxLength='20' required placeholder="Enter title" />
+                    </Form.Group>
+                  </Title>
+                </TitleContainer>
 
                 <Form.Group className="mb-3">
                   <Form.Label>Category</Form.Label>
                   <Form.Select onChange={(e) => setCategory(e.target.value)} required aria-label="Default select example">
-                  <option value="" hidden>Category</option>
+                    <option value="" hidden>Category</option>
                     <option value="Food">Food</option>
-                    <option value="Beverage">Drink</option>
-                    <option value="Fur">Clothing</option>
+                    <option value="Drink">Drink</option>
+                    <option value="Clothing">Clothing</option>
                     <option value="Service">Service</option>
                     <option value="Furniture">Furniture</option>
                     <option value='Electronic'>Electronic</option>
@@ -68,22 +78,43 @@ const AddItemModal = (props) => {
                     <option value="Used">Used</option>
                   </Form.Select>
                 </Form.Group>
+                </LeftSide>
+                </UpperHalf>
 
-                <Form.Group className="mb-3" controlId="formBasicLocation">
-                  <Form.Label>Location</Form.Label>
-                  <Form.Control type="text"  placeholder="Location" required/>
-                </Form.Group>
+                <MidHalf>
+                  <MainAddress>
+                    <Form.Group className="mb-3" controlId="MainAddress">
+                      <Form.Label>Main Address</Form.Label>
+                      <Form.Control type="text" defaultValue={props.userInfo.locations[0].street_name} disabled />
+                    </Form.Group>
+                  </MainAddress>
+
+                  <City>
+                    <Form.Group className="mb-3" controlId="City">
+                      <Form.Label>City</Form.Label>
+                      <Form.Control type="text" defaultValue={props.userInfo.locations[0].city} disabled />
+                    </Form.Group>
+                  </City>
+
+                  <State>
+                    <Form.Group className="mb-3" controlId="State">
+                      <Form.Label>State</Form.Label>
+                      <Form.Control type="text" defaultValue={props.userInfo.locations[0].state} disabled />
+                    </Form.Group>
+                  </State>
+
+                  <ZipCode>
+                    <Form.Group className="mb-3" controlId="ZipCode">
+                      <Form.Label>Zip Code</Form.Label>
+                      <Form.Control type="text" defaultValue={props.userInfo.locations[0].zipcode} disabled />
+                    </Form.Group>
+                  </ZipCode>
+                </MidHalf>
 
 
-              </LeftSide>
-              <RightSide>
-                <Form.Group controlId="formFileLg" className="mb-3">
-                  <Form.Label>Upload your images here</Form.Label>
-                  <Form.Control type="file" size="sm" />
-                </Form.Group>
 
-              </RightSide>
-            </UpperHalf>
+
+
             <LowerHalf>
               <Form.Group className="mb-3" controlId="formBasicDescription">
                 <Form.Label>Description</Form.Label>
@@ -91,16 +122,15 @@ const AddItemModal = (props) => {
               </Form.Group>
 
             </LowerHalf>
-          </FormContainer>
-          <Button variant="primary" type="submit" style={{float: 'right'}} onClick={submitItem}>
-            Submit
-          </Button>
-        </Form>
+            <Button variant="primary" type="submit" style={{ float: 'right' }} onClick={submitItem}>
+              Submit
+            </Button>
+            <Button onClick={props.onHide}>Close</Button>
+          </Form>
+        </FormContainer>
 
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
+
 
     </Modal>
   );
